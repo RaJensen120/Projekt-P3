8425be5cf085779a1e1d9777445a294e3a539a7e
razwaz <andpdud@gmail.com>
2022-10-25 12:13:49 +0200

    classes & main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Projekt_P3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+        
+    }
+}
+
